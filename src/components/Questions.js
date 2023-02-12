7b573a6ca29a4377bb7110f57fc6c935e9117f77
kirtanchandak <themarketingkirtan@gmail.com>
2023-02-12 15:55:41 +0530

    data.json mapping

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,34 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import data from "../database/data";
 
 function Questions() {
   const [checked, setChecked] = useState(undefined);
+  const question = data[0];
+
+  useEffect(() => {
+    console.log(question);
+  });
+
   const onSelect = () => {
     console.log("Selected");
   };
   return (
     <div>
       <div className="flex justify-center items-center">
-        <h2 className="font-medium text-2xl text-gray-200">
-          Simple Question 1{" "}
+        <h2 className="font-medium text-4xl text-gray-200">
+          {question.question}
         </h2>
       </div>
-      <div className="flex justify-center items-center mt-6">
-        <ul>
-          <li>
-            <input
-              id="q1-option"
-              type="radio"
-              value={false}
-              name="options"
-              class="w-4 h-4 "
-            />
-            <label
-              for="default-radio-1"
-              class="ml-2 text-sm font-medium text-gray-200"
-            >
-              Default radio
-            </label>
-          </li>
+      <div className="flex justify-center items-center py-6 mr-40">
+        <ul key={question.id}>
+          {question.options.map((q, index) => (
+            <li key={index}>
+              <input
+                id={`q${index}-option`}
+                type="radio"
+                value={false}
+                name="options"
+                className="w-4 h-4"
+              />
+              <label
+                htmlFor={`q${index}-option`}
+                className="ml-2 font-medium text-gray-200 text-xl"
+              >
+                {q}
+              </label>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
---
+++ src/database/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: "1",
+    question: "My name is ______",
+    options: ["Rahul", "Kush", "Kirtan"],
+  },
+  {
+    id: "2",
+    question: "My name is ______",
+    options: ["Rahul", "Kush", "Kirtan"],
+  },
+  {
+    id: "3",
+    question: "My name is ______",
+    options: ["Rahul", "Kush", "Kirtan"],
+  },
+];
